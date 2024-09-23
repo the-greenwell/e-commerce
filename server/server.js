ccf57eeea3bc5dbc87a552c2ecb0b0266d8f2b13
the-greenwell <ant.greenwell2@gmail.com>
2024-09-23 16:50:18 -0700

    server running with mongodb

--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
-const express = require(express);
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+require('dotenv').config();
 
 const app = express();
 
-const port = 8080;
+app.use(cors());
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MDBURI)
+.then(() => console.log('Connected to DB'))
+.catch(err => console.error('DB connection error: ', err));
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
 })
